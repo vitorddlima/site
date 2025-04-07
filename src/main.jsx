d3a72ae4e5dd186c8b72a847ff4f9f7147b88084
vitorddlima <vitordranka@gmail.com>
2025-04-07 17:00:14 -0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <Analytics router/>
+   <Analytics/>
    <RouterProvider router={router} />
   </StrictMode>
 )
